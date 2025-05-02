53e5fbb9b36f807a140294c0e2e6923b374f7752
Vasu Bhalani <120238034+22cs009vasubhalani@users.noreply.github.com>
2025-05-02 10:59:39 +0530

    Add files via upload

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
